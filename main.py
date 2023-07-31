164916e71f230ca6794a77eac8268d5dffc35c02
Steinway <adrian.montas232@gmail.com>
2023-07-31 16:51:31 +0800

    Test of odd and even

--- main.py
+++ main.py
@@ -8,3 +8,19 @@ def write_numbers_to_file(numbers, filename):
         for number in numbers:
             file.write(str(number) + '\n')
 
+def main():
+    input_file = "numbers.txt"
+    even_output_file = "even.txt"
+    odd_output_file = "odd.txt"
+
+    numbers = read_numbers_from_file(input_file)
+
+    even_numbers = [num for num in numbers if num % 2 == 0]
+    odd_numbers = [num for num in numbers if num % 2 != 0]
+
+    write_numbers_to_file(even_numbers, even_output_file)
+    write_numbers_to_file(odd_numbers, odd_output_file)
+
+if __name__ == "__main__":
+    main()
+
